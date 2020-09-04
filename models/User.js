290b8dfb65c8e72219dd44c08c1bfa6e3cacc59f
jossdz <carloscorrea.dev@gmail.com>
2020-09-04 10:04:47 -0500

    bootstrap + fontawesome + user photo

--- models/User.js
+++ models/User.js
@@ -1,15 +1,43 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String,
+    photo: {
+      type: String,
+      default:
+        "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN", "NONP", "COMPANY", "USER"],
+      default: "USER"
+    },
+    address: String,
+    donatedProducts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Product"
+      }
+    ],
+    industry: String,
+    tickets: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Ticket"
+      }
+    ],
+    website: String
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+)
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+const User = mongoose.model("User", userSchema)
+module.exports = User
